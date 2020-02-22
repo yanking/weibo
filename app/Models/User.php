0558497261b1b648f69e38479d1e0bca12d7ed6e
wangyan <hellowwangyan@163.com>
2020-02-22 16:58:35 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,16 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
@@ -60,5 +70,26 @@ class User extends Authenticatable
     public function feed()
     {
         return $this->statuses()->orderBy('created_at','desc');
+    }
+
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 }
