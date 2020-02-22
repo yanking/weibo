6aa2604e9b386ec5036cf40e630d656b93bbcb3a
wangyan <hellowwangyan@163.com>
2020-02-22 16:13:58 +0800

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,17 +1,32 @@
 @extends('layouts.default')
 @section('title','首页')
 @section('content')
-  <div class="jumbotron">
-    <h1>Hello Laravel</h1>
-    <p class="lead">
-      你现在所看到的是 <a href="#">laravel 入门教程</a> 的示例项目主页。
-    </p>
-    <p>
-      一切，将从这里开始
-    </p>
-    <p>
-      <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
-    </p>
+  @if (Auth::check())
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="jumbotron">
+      <h1>Hello Laravel</h1>
+      <p class="lead">
+        你现在所看到的是 <a href="#">laravel 入门教程</a> 的示例项目主页。
+      </p>
+      <p>
+        一切，将从这里开始
+      </p>
+      <p>
+        <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
+      </p>
 
-  </div>
+    </div>
+  @endif
 @stop
